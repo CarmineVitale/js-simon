c93e5104ec3e53425301f81437c257e13ec0c0a4
CarmineVitale <carmine.vitale90@gmail.com>
2020-04-15 17:40:11 +0200

    RIchiesta esercizio completata

--- js/main.js
+++ js/main.js
@@ -10,10 +10,11 @@ $(document).ready(function () {
     var numeri = [];
     var lista;
     var userNumbers = [];
+    var guessedNum = [];
+    var gioco = false;
 
     for (var i = 0; i < 5; i++) {
         lista = Math.floor(Math.random()* 50) + 1;
-        console.log(lista);
         
         if (!numeri.includes(lista)) {
             numeri.push(lista);
@@ -21,19 +22,41 @@ $(document).ready(function () {
             i = i -1;
         }
     }
+    console.log(numeri);
+    
     alert(numeri);
     setTimeout(function(){
         for (var i = 0; i < 5; i++) {
-            var utente = parseInt(prompt('Inserisci un numero'));
+            var utente = parseInt(prompt('Inserisci un numero da 1 a 50'));
+
+            while (utente < 1 || utente > 50 || isNaN(utente)) {
+                utente = parseInt(prompt('Errore inserimento. Inserisci un NUMERO da 1 a 50'));
+            }
+
             userNumbers.push(utente);
         }
+        for (var k = 0; k < 5; k++) {
+            
+            if (numeri.includes(userNumbers[k])) {
+                guessedNum.push(userNumbers[k]);
+                gioco = true;
+            }
+          
+        }
+        if (gioco == true) {
+            console.log('Il tuo punteggio è ' + guessedNum.length);
+                console.log('I numeri che hai ricordato sono i seguenti: ' + guessedNum);
+        } else if (gioco == false) {
+            console.log('Non hai ricordato nemmeno un numero.MEMORIA DI FERRO!');
+        }
+       
+       
+        
         
     }, 2000);
-    console.log(userNumbers);
     
-
-
-
+       
+    
 
 
 
